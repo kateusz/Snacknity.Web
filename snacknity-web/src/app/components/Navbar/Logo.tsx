c197e0a64d02550d7cf5e4e21fad0e64719ea042
mateuszkulesza <matkul3@gmail.com>
2023-05-29 19:40:49 +0200

    navbar

---
+++ snacknity-web/src/app/components/Navbar/Logo.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      src="/images/logo.png"
+      alt="logo"
+      height="100"
+      width="100"
+      className="hidden md:block cursor-pointer"
+    />
+  );
+};
